75d9f5a2a62dcae23ac8565af08e5a5c22b1501e
Daimen Williams <daimenwill@gmail.com>
2018-02-07 09:53:43 -0800

    uploading file successful, returning image successful, error handling added

--- app/views.py
+++ app/views.py
@@ -1,11 +1,14 @@
-from flask import render_template, request, redirect, flash, url_for, send_from_directory
+from flask import render_template, request, redirect
 from app import app
+from werkzeug.utils import secure_filename
 import os
 
-@app.route('/tmp/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
+ALLOWED_EXTENSIONS = set(["png", "jpg", "jpeg", "gif"])
+UPLOAD_FOLDER = "./app/static/images"
+
+def allow_file(filename):
+    return "." in filename and \
+            filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/", methods=["GET","POST"])
 @app.route("/uploader", methods=["GET","POST"])
@@ -14,24 +17,29 @@ def uploader():
         try:
             #request object must contain a file to upload
             if "file" not in request.files:
-                flash("No file was provided")
-                return redirect(request.url)
+                raise FileNotFoundError("No file was provided")
             #the received file must exist and be valid, let 'photo' = file to upload
             photo = request.files["file"]
             if photo and not photo.filename:
-                flash("Filename does not exist")
-                return redirect(request.url)
+                raise FileNotFoundError("Filename does not exist")
             if photo and allow_file(photo.filename):
                 #ensure file name isn't malicious
                 filename = secure_filename(photo.filename)
                 #add photo to temporary bucket for viewing
-                photo.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
-                return redirect(url_for("uploaded_file",
-                                        filename = filename))
+                photo.save(os.path.join(UPLOAD_FOLDER, filename))
+                return redirect("static/images" + "/" + filename)
+            raise ValueError("filename not allowed")
         #display error information upon unsuccessful processing
         except Exception as e:
-            render_template("500.html",
+            return render_template("error.html",
                             error = str(e),
                             title = "Server Error")
-    return render_template("index.html",
-                            title = "Photo Uploader")
+    else:
+        return render_template("index.html",
+                                title = "Photo Uploader")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("error.html",
+                    error = str(e),
+                    title = "Resource Not Found")
